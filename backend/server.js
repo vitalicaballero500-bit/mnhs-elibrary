01f5f70f619e89f8321a4d30d047061ac90e6912
milord-gitty <milorduchicha@gmail.com>
2026-05-09 17:39:23 +0800

    Security: Whitelisted massive Vercel URL

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://mnhs-elibrary-6r7o.vercel.app'], 
+        origin: [
+            'http://localhost:5173', 
+            'http://127.0.0.1:5173', 
+            'https://mnhs-elibrary-6r7o.vercel.app',
+            'https://mnhs-elibrary-6r7o-ns5taxiad-ruszkin-s-projects.vercel.app'
+        ], 
         methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
         credentials: true
     }
@@ -59,7 +64,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5174',
     'http://localhost:5175', 
     'http://127.0.0.1:5175',
-    'https://mnhs-elibrary.vercel.app', // <-- VERCEL VIP ACCESS
+    'https://mnhs-elibrary-6r7o.vercel.app',
+    'https://mnhs-elibrary-6r7o-ns5taxiad-ruszkin-s-projects.vercel.app',
     process.env.CLIENT_URL 
 ];
 
